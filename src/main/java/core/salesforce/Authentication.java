c9c18fa297b6d139e193c52ad9125106652a1e3c
juanMartinez <juan.martinez@fundacion-jala.org>
2020-03-16 12:41:20 -0400

    authentication class implemented to authenticate sales force user

--- src/main/java/core/salesforce/Authentication.java
+++ src/main/java/core/salesforce/Authentication.java
@@ -9,5 +9,32 @@
 
 package core.salesforce;
 
+import core.utils.AccessToken;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+/**
+ * Allows to authenticate on salesforce.
+ *
+ * @author Juan Martinez.
+ * @version 1.0 16 March 2020.
+ */
 public class Authentication {
+
+    /**
+     * Authentication constructor
+     */
+    private Authentication() {
+
+    }
+
+    /**
+     * Allows to Builds, then Sends authentication request.
+     * @return request specification.
+     */
+    public static RequestSpecification authenticationRequest() {
+        return new RequestSpecBuilder().setBaseUri(AccessToken.getInstance().getClient().getInstanceUrl())
+                .addHeader("Authorization", AccessToken.getInstance().getClient().getTokenType()
+                + " " + AccessToken.getInstance().getClient().getAccessToken()).build();
+    }
 }

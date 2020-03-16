1d3f847ed4bfc1ae595dc330fd6559dd02942903
juanMartinez <juan.martinez@fundacion-jala.org>
2020-03-16 12:37:48 -0400

    manages access token with user values

--- src/main/java/core/utils/AccessToken.java
+++ src/main/java/core/utils/AccessToken.java
@@ -4,27 +4,39 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 
 /**
- * Generates token.
+ * Generates access token.
  * @author Juan Martinez.
  * @version 1.0 16 March 2020.
  */
-public class TokenGenerator {
-    private static TokenGenerator tokenGenerator;
+public class AccessToken {
+    private static AccessToken accessToken;
     private static Response response;
     private static Object jsonResponse;
+    private static Client client;
 
-    private TokenGenerator() {
-        initializeTokenGenerator();
+    /**
+     * Constructor to initialize AccessToken.
+     */
+    private AccessToken() {
+        initializeAccessToken();
     }
 
-    public static TokenGenerator getInstance() {
-        if (tokenGenerator == null) {
-            tokenGenerator = new TokenGenerator();
+    /**
+     * Gets instance of AccessToken.
+     * @return AccessToken instance.
+     */
+    public static AccessToken getInstance() {
+        if (accessToken == null) {
+            accessToken = new AccessToken();
+            client = new Client();
         }
-        return tokenGenerator;
+        return accessToken;
     }
 
-    private void initializeTokenGenerator()  {
+    /**
+     * Initializes AccessToken.
+     */
+    private void initializeAccessToken()  {
         String userToken = GradleReader.getInstance().getUser().getToken();
         response = given()
                 .param("username", GradleReader.getInstance().getUser().getUsername())
@@ -35,9 +47,27 @@ public class TokenGenerator {
                 .param("client_secret", GradleReader.getInstance().getUser().getClientSecret())
                 .when().post(GradleReader.getInstance().getUser().getAuthUrl());
         jsonResponse = response.getBody();
+        setClient(jsonResponse);
     }
 
-    public Object getJsonResponse() {
-        return jsonResponse;
+    /**
+     * Sets client values.
+     */
+    private void setClient(final Object jsonResponse) {
+        client.setAccessToken("");
+        client.setInstanceUrl("");
+        client.setId("");
+        client.setTokenType("");
+        client.setIssuedAt("");
+        client.setSignature("");
+        client.setAuthUrl("");
+    }
+
+    /**
+     * Gets client instance.
+     * @return
+     */
+    public Client getClient() {
+        return client;
     }
 }

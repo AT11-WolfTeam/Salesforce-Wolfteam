a182bb6e5d1d3e72f6fd869edc1cf6695125d0f6
Alan Mihael Escalera <alan.escalera@fundacion-jala.org>
2020-03-19 20:55:52 -0400

    opportunities

--- src/main/java/salesforce/ui/pages/opportunities/OpportunitiesPageAbstract.java
+++ src/main/java/salesforce/ui/pages/opportunities/OpportunitiesPageAbstract.java
@@ -1,4 +1,25 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
 package salesforce.ui.pages.opportunities;
 
-public class NewOpportunitiesPageAbstract {
+import salesforce.ui.pages.BasePage;
+
+/**
+ * Defines OpportunitiesPageAbstract.
+ *
+ * @author Alan Escalera.
+ * @version 1.0 19 March 2020.
+ */
+public abstract class OpportunitiesPageAbstract extends BasePage {
+    @Override
+    protected void waitUntilPageObjectIsLoaded() {
+
+    }
 }

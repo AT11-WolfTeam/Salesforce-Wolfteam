d258faf88e8717d3c7d880cc9ad3eb8802631e15
Enrique <Enrique.Carrizales@fundacion-jala.org>
2020-03-22 22:36:31 -0400

    implementation of toast

---
+++ src/main/java/salesforce/ui/components/span/ToastMessageSpan.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package salesforce.ui.components.span;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import salesforce.ui.pages.BasePage;
+
+/**
+ * Manages a ToastMessageSpan.
+ *
+ * @author Enrique Carrizales.
+ * @version 1.0 22 March 2020.
+ */
+public class ToastMessageSpan extends BasePage {
+
+    @FindBy(css = "span[class='toastMessage slds-text-heading--small forceActionsText']")
+    private WebElement toastMessage;
+
+    @Override
+    protected void waitUntilPageObjectIsLoaded() {
+        webDriverWait.until(ExpectedConditions.visibilityOf(toastMessage));
+    }
+
+    /**
+     * Returns a toast message.
+     * @return String value.
+     */
+    public String getToastMessage() {
+        System.out.println(toastMessage.getText());
+        return toastMessage.getText();
+    }
+}

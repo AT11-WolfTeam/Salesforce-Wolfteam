d8eab50992a9dc33701b6b12deef50662515c81f
Enrique <Enrique.Carrizales@fundacion-jala.org>
2020-03-27 12:06:29 -0400

    Add class

---
+++ src/main/java/salesforce/ui/components/span/AbstractToastUpdateMessageSpan.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package salesforce.ui.components.span;
+
+import salesforce.ui.pages.AbstractBasePage;
+
+/**
+ * Manages AbstractToastUpdateMessageSpan.
+ *
+ * @author Enrique Carrizales.
+ * @version 1.0 27 March 2020.
+ */
+public abstract class AbstractToastUpdateMessageSpan extends AbstractBasePage {
+
+    @Override
+    protected void waitUntilPageObjectIsLoaded() {
+
+    }
+}
